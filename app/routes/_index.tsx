8c13673e6cc85244fbf6d76667b6f54900267018
thedevbrian1 <brayomwas95@gmail.com>
2024-12-16 13:50:03 +0300

    Search locations and show pending ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
-import { Search } from "~/icon";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import {
+  Form,
+  isRouteErrorResponse,
+  Link,
+  useLoaderData,
+  useNavigation,
+  useRouteError,
+} from "@remix-run/react";
+import { ErrorIcon, Search, ThreeDots } from "~/icon";
 
 export const meta: MetaFunction = () => {
   return [
@@ -53,9 +60,22 @@ interface WeatherResponse {
   };
 }
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
+  let url = new URL(request.url);
+
+  let searchParams = url.searchParams;
+
+  let city = searchParams.get("q") || "Nairobi";
+
+  // if (!city) {
+  //   throw new Response("City name not found!", {
+  //     status: 400,
+  //     statusText: "Bad Request",
+  //   });
+  // }
+
   let weatherRes = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Kisumu&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
   );
   let weather: WeatherResponse = await weatherRes.json();
 
@@ -64,19 +84,37 @@ export async function loader() {
 
 export default function Index() {
   let weather = useLoaderData<typeof loader>();
-  console.log({ weather });
+
+  let navigation = useNavigation();
+
+  let isSearching = Boolean(
+    navigation.state === "loading" && navigation.location.search
+  );
 
   let locations = ["Mombasa", "Tokyo", "Cairo", "Helsinki"];
 
   return (
-    <main className="h-screen w-full bg-[url('https://images.unsplash.com/photo-1520259137323-f15f8cbaa61f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-center grid lg:grid-cols-5">
+    <main
+      className={`h-screen w-full relative bg-[url('https://images.unsplash.com/photo-1520259137323-f15f8cbaa61f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-center grid lg:grid-cols-5`}
+    >
+      {/* Show pending UI */}
+      {isSearching ? (
+        <div className="w-full h-screen absolute inset-0 grid place-items-center bg-black/50">
+          <span className="w-20">
+            <ThreeDots />
+          </span>
+        </div>
+      ) : null}
+
       <div className="lg:col-span-3 flex flex-col justify-between p-8 lg:pb-24">
         {/* Weather */}
         <h1>Weather</h1>
-        <div className="flex gap-4 items-center">
-          <span className="text-4xl lg:text-6xl font-semibold">16 &deg;</span>
-          <span className="text-2xl">London</span>
-          <span>Cloudy</span>
+        <div className="flex gap-4 items-end">
+          <span className="text-4xl lg:text-6xl font-semibold">
+            {weather.main.temp} &deg;<span className="lg:text-4xl">C</span>
+          </span>
+          <span className="text-2xl">{weather.name}</span>
+          <span>{weather.weather[0].main}</span>
         </div>
       </div>
       <div className="backdrop-blur-md lg:col-span-2 px-4 lg:px-5">
@@ -112,4 +150,54 @@ export default function Index() {
       </div>
     </main>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    console.error(error);
+    return (
+      <div className="w-full h-screen grid place-items-center">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="w-60">
+            <ErrorIcon />
+          </div>
+          <h1 className="text-3xl text-red-500">
+            {error.status} {error.statusText}
+          </h1>
+          <p className="text-red-300">{error.data}</p>
+          <Link
+            to="."
+            prefetch="intent"
+            className="bg-white hover:bg-gray-300 transition ease-in-out duration-300 px-4 py-2 rounded-md text-black"
+          >
+            Try again
+          </Link>
+        </div>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    console.error(error);
+    return (
+      <div className="w-full h-screen grid place-items-center">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="w-60">
+            <ErrorIcon />
+          </div>
+          <h1 className="text-3xl text-red-500">Error</h1>
+          <p className="text-red-300">{error.message}</p>
+          <Link
+            to="."
+            prefetch="intent"
+            className="bg-white hover:bg-gray-300 transition ease-in-out duration-300 px-4 py-2 rounded-md text-black"
+          >
+            Try again
+          </Link>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
